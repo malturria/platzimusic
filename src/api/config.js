3e1110e467b7046e8ea6e588a8d36b6399423f25
malturria <mauroalturria@gmail.com>
2018-03-18 20:53:06 -0300

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,7 @@
+export default {
+	appName : 'platzimusic',
+	apiKey : '34d40a68fe4ba7e28162fb0cdc211827',
+	secret : 'bc19ec476d330244dd4fe1893b89595f',
+	registeredTo : 'malturria'
+
+}
---
+++ src/components/Artist.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  
+  li
+    h2: a(v-bind:href="artist.url" target="_blank") {{artist.name}}
+    img(v-bind:src="artist.image[2]['#text']")
+
+</template>
+
+<script>
+
+export default {
+  name: 'artist',
+  props: ['artist']
+ 
+}
+ 
+</script>
+
+<style lang="stylus" scoped>
+li
+  display block
+  margin 10px 0
+</style>
